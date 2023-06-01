7c07c34b5b31d4a90da1aa91e24cd07842a3934c
Alan Hoffer <alanhoffer97@hotmail.com>
2023-05-31 21:45:35 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
@@ -7,9 +8,31 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 
 function App() {
+
+  const [position, setPosition] = useState(1)
+      
+  const scrollEvent = (event) => {
+    const main = event.target;
+    const mainSize = main.clientHeight;
+  
+    if (main.scrollTop < mainSize) {
+        setPosition(1);
+    }
+    if (main.scrollTop >= mainSize && main.scrollTop < (mainSize * 2)) {
+        setPosition(2);
+    }
+    if (main.scrollTop >= (mainSize * 2) && main.scrollTop < (mainSize * 3 )) {
+        setPosition(3);
+    }
+    if (main.scrollTop >= (mainSize * 3 )) {
+        setPosition(4);
+    }      
+  
+  };
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className="App" onScroll={(event) => scrollEvent(event)}>
+      <Navbar props={position} />
       <Header />
       <Home />
       <About />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,15 @@ import '../assets/css/Navbar.css';
 import welcomeImage from '../assets/images/asd.png'
 
 
-function Navbar() {
+function Navbar({props}) {
+
     return (
         <nav className="Navbar">
             <ul>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#about'>About</a></li>
-                <li><a href='#skills'>Skills</a></li>
-                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#home' className={props == 1 ? 'active': ''}>HOME</a></li>
+                <li><a href='#about' className={props == 2 ? 'active': ''}>ABOUT</a></li>
+                <li><a href='#skills' className={props == 3 ? 'active': ''}>SKILLS</a></li>
+                <li><a href='#projects' className={props == 4 ? 'active': ''}>PROJECTS</a></li>
             </ul>
         </nav>
     );
