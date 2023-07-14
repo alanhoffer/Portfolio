81706a73a7646b55f63d5b6857211deb6469937b
Alan Hoffer <alanhoffer97@hotmail.com>
2023-07-14 13:56:19 -0300

    added rustamiel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -92,7 +92,7 @@ function Projects() {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                             </svg>
-                            <img alt='' src='https://i.imgur.com/Jk3pjRv.png' />
+                            <img alt='' src='https://i.imgur.com/MDxFQKz.png' />
                         </div>
                     </div>
                 </div>
